090a2495f37c51aeb8e03d5a22319aebd2b49805
JongMany <blackberry1114@naver.com>
2024-09-01 17:22:18 +0900

    refactor: main에서 사용되는 컴포넌트 모듈처리

--- client/src/app/(main)/main/page.tsx
+++ client/src/app/(main)/main/page.tsx
@@ -1,10 +1,7 @@
-// import Description from "@/app/(main)/main/_components/Description";
-// import HeadTitle from "@/app/(main)/main/_components/HeadTitle";
-
 // Framer Motion - https://www.youtube.com/watch?v=nPsJfTVjkdo
-import HeadTitle from "@components/main/HeadTitle";
-import Description from "@components/main/Description";
-
+// import HeadTitle from "@components/main/HeadTitle";
+// import Description from "@components/main/Description";
+import {HeadTitle, Description} from '@components/main';
 export default function Page() {
   return (
     <>
