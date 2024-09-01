8a1e3403afad4246d451917d25f79fca825af1af
JongMany <blackberry1114@naver.com>
2024-09-01 17:14:49 +0900

    refactor: main 화면 컴포넌트 components 폴더로 이동하는 작업

--- client/src/app/(main)/main/page.tsx
+++ client/src/app/(main)/main/page.tsx
@@ -1,7 +1,10 @@
-import Description from "@/app/(main)/main/_components/Description";
-import HeadTitle from "@/app/(main)/main/_components/HeadTitle";
+// import Description from "@/app/(main)/main/_components/Description";
+// import HeadTitle from "@/app/(main)/main/_components/HeadTitle";
 
 // Framer Motion - https://www.youtube.com/watch?v=nPsJfTVjkdo
+import HeadTitle from "@components/main/HeadTitle";
+import Description from "@components/main/Description";
+
 export default function Page() {
   return (
     <>
