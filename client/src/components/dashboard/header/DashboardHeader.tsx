d2a7c925f1c6f080b163be63953597e70c424c84
JongMany <blackberry1114@naver.com>
2024-09-03 07:17:21 +0900

    refactor: 불필요한 컴포넌트 쪼개기 제거

--- client/src/components/dashboard/header/DashboardHeader.tsx
+++ client/src/components/dashboard/header/DashboardHeader.tsx
@@ -1,10 +1,24 @@
-import DurationSelector from "@components/dashboard/header/DurationSelector";
+"use client";
 
+import React from "react";
+import {useDuration} from "@/store/duration/useDuration";
 
 export default function DashboardHeader() {
+  const {duration, setDuration} = useDuration();
+
+  const changeDurationHandler = (e: React.ChangeEvent<HTMLSelectElement>) => {
+    const duration = e.target.value as "WEEK" | "MONTH" | "YEAR";
+    setDuration(duration);
+  };
+
   return (
-    <nav className="py-4 px-2">
-      <DurationSelector />
-    </nav>
+      <nav className="py-4 px-2">
+        <select defaultValue={duration} onChange={changeDurationHandler}
+                className="text-[20px] font-semibold rounded-md px-6 py-1 border-4 border-black">
+          <option value="WEEK">이번 주</option>
+          <option value="MONTH">이번 달</option>
+          <option value="YEAR">올해</option>
+        </select>
+      </nav>
   );
 }
