1ad7de05c848ec80211bdd6f0e5c0c975b7b3e0a
JongMany <blackberry1114@naver.com>
2024-09-03 08:17:15 +0900

    refactor: 전체 랭킹 컴포넌트 폴더명 수정 및 아키텍처 수정

--- client/src/components/rank/total-rank/RankContainer.tsx
+++ client/src/components/rank/total-rank/RankContainer.tsx
@@ -1,9 +1,9 @@
 "use client";
 
-import RankCard from "@components/rank/show-rank/RankCard";
-import DailyRanking from "@components/rank/show-rank/DailyRanking";
-import WeeklyRanking from "@components/rank/show-rank/WeeklyRanking";
-import MonthlyRanking from "@components/rank/show-rank/MonthlyRanking";
+import RankCard from "@components/rank/total-rank/wrapper/RankCard";
+import DailyRanking from "@components/rank/total-rank/periodic-rank/DailyRanking";
+import WeeklyRanking from "@components/rank/total-rank/periodic-rank/WeeklyRanking";
+import MonthlyRanking from "@components/rank/total-rank/periodic-rank/MonthlyRanking";
 import useQueryRankData from "@hooks/rank/useQueryRankData";
 
 export default function RankContainer() {
